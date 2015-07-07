f4038202057716e9f08efa7470b881a684e1304a
Sam Desborough <sam.desborough@gmail.com>
2015-07-07 13:15:03 +0100

    add localstorage defaults

--- static/src/javascripts/projects/common/modules/locale/locale.js
+++ static/src/javascripts/projects/common/modules/locale/locale.js
@@ -1,6 +1,19 @@
 import $ from 'common/utils/$';
 import localise from './localise';
+import storage from 'common/utils/storage';
+
+const localStorage = storage.local;
+const key = 'localise.hackday';
+const defaults = {
+    "currency": "USD",
+    "distance": ["in", "ft", "mi"],
+    "weight": ["oz", "lb", "ton"]
+};
 
 export default function() {
+    if (!localStorage.get(key)) {
+        localStorage.set(key, defaults);
+    }
+
     $('p').map((el) => localise($(el)))
 };
