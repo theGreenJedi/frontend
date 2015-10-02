b7fb8494e50367509c7d2bb91c2ef49722508f18
Sam Desborough <sam.desborough@gmail.com>
2015-10-02 11:51:36 +0100

    Add iOS sticky grid function

--- static/src/javascripts/es6/projects/common/modules/crosswords/sticky-wrapper.js
+++ static/src/javascripts/es6/projects/common/modules/crosswords/sticky-wrapper.js
@@ -2,6 +2,7 @@ import React from 'react';
 import fastdom from 'fastdom';
 
 import $ from 'common/utils/$';
+import detect from 'common/utils/detect';
 import mediator from 'common/utils/mediator';
 
 // $gs-baseline (px)
@@ -14,8 +15,39 @@ export default class StickyWrapper extends React.Component {
     }
 
     componentDidMount() {
+        const handler = (() => {
+            if (detect.isIOS()) {
+                this.$node.css('position', 'absolute');
+                return this.stickIOS;
+            }
+
+            return this.stick;
+        })().bind(this);
+
         this.$node = $(React.findDOMNode(this.refs.game));
-        mediator.on('crosswords:scroll', this.stick.bind(this));
+
+        mediator.on('crosswords:scroll', (offsets, scrollY) => {
+            return detect.getBreakpoint() === 'tablet' && handler(offsets, scrollY);
+        });
+    }
+
+    stickIOS(offsets, scrollY) {
+        const scrollOffset = scrollY + baseline - offsets.container.top;
+        const above = scrollOffset < 0;
+        const below = scrollOffset + offsets.game.height >= offsets.container.height;
+
+        /**
+         * Determine the y point for the grid:
+         *     if we've scrolled above the container, it's 0
+         *     if we're below the container, it's the lowest possible point
+         *     otherwise we're in the the sticky zone, so it follows the scroll offset
+         */
+        const y
+            = above ? 0
+            : below ? offsets.container.height - offsets.game.height
+            : scrollOffset;
+
+        this.$node.css('top', y);
     }
 
     stick(offsets, scrollY) {
