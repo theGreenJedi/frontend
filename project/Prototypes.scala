90bb6abb099247773dca3d57ba542aee7e7ec7f2
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-02-10 13:56:56 +0000

    Update deprecated code

--- project/Prototypes.scala
+++ project/Prototypes.scala
@@ -1,16 +1,15 @@
 package com.gu
 
-import com.gu.versioninfo.VersionInfo
-import com.typesafe.sbt.packager.universal.UniversalPlugin
-import sbt._
-import sbt.Keys._
-import com.typesafe.sbt.web.SbtWeb.autoImport._
-import com.typesafe.sbt.SbtNativePackager._
-import com.typesafe.sbt.packager.Keys._
+import com.gu.Dependencies._
 import com.gu.riffraff.artifact.RiffRaffArtifact
 import com.gu.riffraff.artifact.RiffRaffArtifact.autoImport._
+import com.gu.versioninfo.VersionInfo
+import com.typesafe.sbt.SbtNativePackager._
+import com.typesafe.sbt.packager.Keys._
+import com.typesafe.sbt.packager.universal.UniversalPlugin
 import play.twirl.sbt.Import._
-import Dependencies._
+import sbt.Keys._
+import sbt._
 
 trait Prototypes {
   val version = "1-SNAPSHOT"
@@ -146,12 +145,12 @@ trait Prototypes {
     }
   )
 
-  def root() = Project("root", base = file(".")).enablePlugins(play.PlayScala)
+  def root() = Project("root", base = file(".")).enablePlugins(play.sbt.Play)
     .settings(frontendCompilationSettings)
     .settings(frontendRootSettings)
 
   def application(applicationName: String) = {
-    Project(applicationName, file(applicationName)).enablePlugins(play.PlayScala, RiffRaffArtifact, UniversalPlugin)
+    Project(applicationName, file(applicationName)).enablePlugins(play.sbt.Play, RiffRaffArtifact, UniversalPlugin)
     .settings(frontendDependencyManagementSettings)
     .settings(frontendCompilationSettings)
     .settings(frontendClientSideSettings)
@@ -162,7 +161,7 @@ trait Prototypes {
   }
 
   def library(applicationName: String) = {
-    Project(applicationName, file(applicationName)).enablePlugins(play.PlayScala)
+    Project(applicationName, file(applicationName)).enablePlugins(play.sbt.Play)
     .settings(frontendDependencyManagementSettings)
     .settings(frontendCompilationSettings)
     .settings(frontendClientSideSettings)
