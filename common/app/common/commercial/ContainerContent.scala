70ed9641cdde9b3fc0d6fa31c5ec385f2a20fd59
Kelvin Chappell <kelvin.chappell@theguardian.com>
2016-03-18 09:52:17 +0000

    Add create method for ContainerContent

--- common/app/common/commercial/ContainerContent.scala
+++ common/app/common/commercial/ContainerContent.scala
@@ -1,10 +1,17 @@
 package common.commercial
 
+import model.facia.PressedCollection
+
 case class ContainerContent(
   title: String,
   targetUrl: String,
   trails: Seq[CommercialTrail]
 )
+
+object ContainerContent {
+
+  def fromPressedCollection(collection: PressedCollection): ContainerContent = ???
+}
 
 case class CommercialTrail(
   headline: String,
